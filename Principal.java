f6094c5b1d45ca6d86184521f974077676c9fae2
Ocarlls <ra126610@uem.br>
2022-06-19 15:18:17 -0300

    Refatoração do método criaConta

--- Principal.java
+++ Principal.java
@@ -10,10 +10,17 @@ public class Principal {
 		ContaPoupanca contaPoupanca = new ContaPoupanca("1209", "1101", clienteFisico, "1878", 15);
 		ContaEspecial contaEspecial = new ContaEspecial("1210", "5522", clienteJuridico, "1878", 2000);
 		
+		criaConta(clienteFisico);
 	}
 	
 	public static void criaConta(Pessoa cliente) {
-		System.out.println("Digite o nome do cliente.");
-		JOptionPane.showInputDialog("Digite o nome do cliente");
+		cliente.setNome(JOptionPane.showInputDialog("Digite o nome do cliente"));
+		cliente.setTelefone(JOptionPane.showInputDialog("Digite o telefone do cliente"));
+		cliente.setEndereço(JOptionPane.showInputDialog("Digite o endereço do cliente"));
+		if (cliente instanceof PessoaFisica) {
+			((PessoaFisica) cliente).setCpf(JOptionPane.showInputDialog("Digite o cpf do cliente"));
+		} else {
+			((PessoaJuridica) cliente).setCnpj(JOptionPane.showInputDialog("Digite o cnpj do cliente"));
+		}
 	}
 }
